7a2572807cc99e02f55fd48255816fa0bbba7a59
almondaja <102125154+almondaja@users.noreply.github.com>
2026-09-09 17:27:28 +0700

    Restrict CORS to Apollo GraphQL Studio origin

--- graphql-vercel/next.config.js
+++ graphql-vercel/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         source: "/api/graphql",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://studio.apollographql.com" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
@@ -14,4 +14,5 @@ const nextConfig = {
     ]
   }
 };
+
 module.exports = nextConfig;
